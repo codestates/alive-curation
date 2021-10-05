7e88fc81f082fe81998cca55d5fbc470650dcd2c
Ellery Vernon <tmpks3@gmail.com>
2021-10-05 14:06:36 +0900

    server init

---
+++ server/app.js
@@ -0,0 +1,39 @@
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+import morgan from "morgan"
+import hpp from "hpp"
+import helmet from "helmet"
+import cookieParser from "cookie-parser"
+import mongoose from "mongoose"
+
+dotenv.config()
+
+const app = express()
+const dev = process.env.NODE_ENV !== "production"
+
+mongoose
+	.connect(process.env.MONGO_URI)
+	.then(console.log("MongoDB Connected!!"))
+	.catch((err) => console.error(err))
+
+if (dev) {
+	app.use(morgan("dev"))
+	app.use(cors({ origin: true, credentials: true }))
+} else {
+	app.use(morgan("combined"))
+	app.use(hpp())
+	app.use(helmet())
+	app.use(cors({ origin: "site-url", credentials: true }))
+}
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser(process.env.COOKIE_SECURE))
+
+app.get("/", (req, res) => {
+	res.status(200).send("hello")
+})
+
+const port = dev ? 8080 : process.env.PORT
+app.listen(port, () => console.log(`sever is ready on port: ${port}, terminate to Control + C.`))
