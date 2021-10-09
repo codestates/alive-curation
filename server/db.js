c4cdb898e14ff0c677b0c15f358a81d68b616e1d
usreon <sonyeonju7@gmail.com>
2021-10-09 21:22:51 +0900

    feat: Add db.js

--- server/db.js
+++ server/db.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose")
-const dotenv = require("dotenv")
-
-dotenv.config()
+const { Posts } = require("./models/Posts")
 
 if (!process.env.MONGO_URI) {
 	console.error("MongoDB connection string is missing!")
