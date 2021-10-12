af219c66045b025e1b6a0118e1a75d771e583593
usreon <sonyeonju7@gmail.com>
2021-10-12 15:38:02 +0900

    feat: Add middleware for function

--- server/routes/posts.route.js
+++ server/routes/posts.route.js
@@ -4,7 +4,7 @@ const { isLoggedIn, isAdmin } = require("../lib/middleware")
 
 const router = express.Router();
 
-router.post('/posts', addPosts);
+router.post('/posts', isLoggedIn, isAdmin, addPosts);
 router.get('/posts', getAllPosts);
 router.get('/posts/:post_id', getPosts);
 router.delete('/posts/:post_id', isLoggedIn, isAdmin, deletePosts);
