34a5d2a6a934952f8c960899777b74652f2d8b0a
usreon <sonyeonju7@gmail.com>
2021-10-12 11:06:19 +0900

    docs: Delete this file

--- server/controllers/book.js
+++
@@ -1,9 +0,0 @@
-const Book = require('../models/Book');
-require('dotenv').config();
-
-module.exports = {
-  getPostsData: async (req, res) => {
-    // query = /book?title
-    console.log(req.params)
-  }
-}
